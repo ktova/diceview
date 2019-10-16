fa34fed660182c577ca6e91ec5fa53310c92fbba
Tonva07 <teva@2gik.ch>
2019-10-16 15:17:55 +0200

    Les fonctions marchent , fuck virgile
    sale chien

--- diceapp.php
+++ diceapp.php
@@ -1,7 +1,5 @@
 <?php
 
-// Definition des fonctions
-
 class Dice
 {
     public $face;
@@ -17,23 +15,8 @@ class Dice
         $this->kept = $k;
     }
 
-    // Getters and Setters
-    function getJets()
-    {
-        return $this->nbjets;
-    }
-
-    function getRel()
-    {
-        return $this->seuilrel;
-    }
-
-    function getKept()
-    {
-        return $this->kept;
-    }
-
     // Méthode du jet de dés
+
     function playDice()
     {
         $n = $this->nbjets;
@@ -43,42 +26,113 @@ class Dice
         $brutvalue = 0;
         $montant = array();
         //debug
-        echo 'Valeur i='.$i . "<br>" . 'Valeur Jets='.$n . "<br>" . 'Valeur Relance='.$m . "<br>". 'Valeur Conserve='.$k . "<br><br>";
+        // echo 'Valeur i='.$i . "<br>" . 'Valeur Jets='.$n . "<br>" . 'Valeur Relance='.$m . "<br>". 'Valeur Conserve='.$k . "<br><br>";
         for ( $i; $i <= $n; $i++ ) {
             $f = rand ( 1, 10 );
-            // Debug
-            echo 'valeur sf base: '.$f.' - ';
+            //debug
+            // echo 'valeur sf base: '.$f.' - ';
             $val += $f;
             while ( $f >= $m ) {
+                $f = 0;
                 $f = rand ( 1, 10 );
                 //debug
-                echo '/'.$f.'/';
+                // echo '/'.$f.'/';
                 $val += $f;
-                $f = 0;
             }
             array_push ( $montant, $val );
             //debug
-            echo $i . " <-Index . Value -> " . $val . '<br>';
+            // echo "/  Index :". $i . "// Value -> " . $val . '<br>';
             $val = 0;
         }
         rsort ( $montant );
-        echo '<br>';
+        //echo '<br>';
         //debug
-        print_r ($montant);
-        echo '<br>';
-        echo '<br>';
+        // print_r ($montant);
+        //echo '<br>';
+        //echo '<br>';
         for ($x=0;$x < $k;$x++) {
             $brutvalue += $montant[$x];
         }
         //debug
-        echo 'Valeur ktotale : '.$brutvalue;
+        // echo 'Valeur ktotale :';
+        // echo $brutvalue;
+        return $brutvalue;
     }
-
 }
 
-// Tester un lancé
+// Jouer les probabilités approximatives
 
-$jet1 = new Dice( 10, 5, 3 );
-echo $jet1->playDice ();
+$gJets = 10000;
+$results = [];
+for($i = 1; $i < $gJets; ++$i){
+    $jet[$i] = new Dice(6,6,3);
+    $y = $jet[$i]->playDice();
+    array_push ($results,$y);
+}
+
+// Probabilité des occurrences pour n nombre
+
+$compteur = array_count_values ($results);
+ksort ($compteur);
+$tojson = [];
+foreach($compteur as $khey => $valou){
+    array_push ($tojson,['['.$khey.','.$valou.']']);
+}
+//print_r ($tojson);
+// Conversion pour le json
+$cleanjson1 = [];
+foreach ($tojson as $kle => $kval){
+    foreach ($kval as $skle => $sval){
+        array_push ($cleanjson1,$sval);
+    }
+}
+json_encode ($cleanjson1, JSON_FORCE_OBJECT);
+echo 'Données stastiques des prévisions approximatives pour les occurences de n jusqua n+x :<br>';
+print_r ($cleanjson1);
+
+//$cleanjson1 = array_values ($compteur);
+//$graph1json = json_encode ($compteur,JSON_FORCE_OBJECT);
+//print_r ($graph1json);
+
+//debug
+//foreach ($compteur as $indx => $val){
+//    echo '['.$indx.','.$val.'],<br>';
+//}
+
+//--------------------------------------------------
+
+// Probabilité pour qu'une occurrence sois > n nombre
+
+$nprob = [];
+$nfirst = array_key_first ($compteur);
+$nlast = array_key_last ($compteur);
+for ($z = 0; $z < $nlast; ++$z){
+    $sd = 0;
+    foreach ($compteur as $nomb => $occur){
+        if ($nomb < $z) {
+            $sd+= 0;
+        } else {
+            $sd += $occur;
+        }
+    }
+    if ($z < $nfirst){
+        $nperc = 100;
+    } else {
+        $nperc = ( $sd / $gJets ) * 100;
+    }
+    array_push ($nprob, $nperc);
+}
+
+// Conversion pour le json
+
+$cleanjson2 = [];
+foreach ($nprob as $thenum => $theperc){
+    array_push ($cleanjson2,'['.$thenum.','.$theperc.']');
+}
+echo '<br>Données stastiques des prévisions approximatives pour les probabilités que r soit > n :<br>';
+print_r ($cleanjson2);
+
+
+//--------------------------------------------------
 
 ?>
