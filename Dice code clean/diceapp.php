8437aa7fb1845443c3d6c23c9c34602997b666ab
Tonva07 <teva@2gik.ch>
2019-10-17 11:56:00 +0200

    Code propre , pas opti mais pg

---
+++ Dice code clean/diceapp.php
@@ -0,0 +1,106 @@
+<?php
+
+class Dice
+{
+    public $nbjets;
+    public $seuilrel;
+    public $kept;
+
+    function __construct($n,$m,$k)
+    {
+        $this -> nbjets = $n;
+        $this -> seuilrel = $m;
+        $this -> kept = $k;
+    }
+
+    function playDice()
+    {
+        $n = $this -> nbjets;
+        $m = $this -> seuilrel;
+        $k = $this -> kept;
+        $i = 1;
+        $val = 0;
+        $brutvalue = 0;
+        $montant = array();
+        for ($i; $i <= $n; $i++) {
+            $f = rand(1,10);
+            $val += $f;
+            while ($f >= $m) {
+                $f = rand(1,10);
+                $val += $f;
+            }
+            array_push($montant,$val);
+            $val = 0;
+        }
+        rsort($montant);
+        for ($x = 0; $x < $k; $x++) {
+            $brutvalue += $montant[ $x ];
+        }
+        return $brutvalue;
+    }
+}
+
+if ($_POST[ 'nbDices' ]) {
+    if ($_POST[ 'keep' ]) {
+        if ($_POST[ 'reroll' ]) {
+
+            $vnbdice =  (int)$_POST[ 'nbDices' ];
+            $vkeep = (int)$_POST[ 'keep' ];
+            $vreroll = (int)$_POST[ 'reroll' ];
+
+            $gJets = 100000;
+            $results = [];
+            for ($i = 1; $i < $gJets; ++$i) {
+                $jet[ $i ] = new Dice($vnbdice,$vreroll,$vkeep);
+                $y = $jet[ $i ] -> playDice();
+                array_push($results,$y);
+            }
+
+            $compteur = array_count_values($results);
+            ksort($compteur);
+            $tojson = [];
+            foreach ($compteur as $khey => $valou) {
+                array_push($tojson,['[' . $khey . ',' . $valou . ']']);
+            }
+
+            $cleanjson1 = [];
+            foreach ($tojson as $kle => $kval) {
+                foreach ($kval as $skle => $sval) {
+                    array_push($cleanjson1,$sval);
+                }
+            }
+
+            $nprob = [];
+            $nfirst = array_key_first($compteur);
+            $nlast = array_key_last($compteur);
+            for ($z = 0; $z < $nlast; ++$z) {
+                $sd = 0;
+                foreach ($compteur as $nomb => $occur) {
+                    if ($nomb < $z) {
+                        $sd += 0;
+                    } else {
+                        $sd += $occur;
+                    }
+                }
+                if ($z < $nfirst) {
+                    $nperc = 100;
+                } else {
+                    $nperc = ($sd / $gJets) * 100;
+                }
+                array_push($nprob,$nperc);
+            }
+
+
+            $cleanjson2 = [];
+            foreach ($nprob as $thenum => $theperc) {
+                array_push($cleanjson2,'[' . $thenum . ',' . $theperc . ']');
+            }
+            json_encode($cleanjson2,JSON_FORCE_OBJECT,JSON_PRETTY_PRINT);
+        } else {
+        }
+    } else {
+    }
+} else {
+}
+
+?>
---
+++ Dice code clean/template.php
@@ -0,0 +1,51 @@
+<?php 
+$header = 
+'<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>re.teva</title>
+        <meta name="description" content="on est la">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" 
+            type="image/png" 
+            href="/static/favicontk.png">
+        <link rel="stylesheet" href="/css-js/tkstyle.css">
+        <link rel="stylesheet" href="/css-js/csshtcours.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script> 
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="https://kit.fontawesome.com/0d2059c859.js" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+        <script type="text/javascript" src="/css-js/tkjs.js"></script>
+        <script type="text/javascript" src="/css-js/sticky.js"></script>
+    </head>
+    <div id="topbar" class="main-navbar ax-flex ax-alin">
+    <div id="fclogo"><img id="mainlogo homeboy" class="ax-abso-mid ax-sq75" src="/static/mainlogo.svg"></div>
+
+    <div class="btn-group">
+            <button id="menubtn" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <i class="far fa-angry"></i>
+            </button>
+            <div class="dropdown-menu dropdown-menu-right">
+              <a href="https://teva.re" target="_blank"><button class="dropdown-item homeboy" type="button">Teva.re</button></a>
+              <button class="dropdown-item disabled" type="button">Hugo.tv</button>
+              <button class="dropdown-item disabled" type="button">eTron.gg</button>
+            </div>
+          </div>
+    </div>'
+?>
+
+<?php
+$footer = '
+    <footer>
+    <div id="footer" class="ax-flex ax-jcen-mid ax-wrap-col">
+        <div id="copyright" class="ax-flex ax-jcen"><h6>© TK 2019 v1 - Powered by FC Fumier</h6></div>
+        </div>
+    </footer>
+    </body>
+</html>
+'
+?>
